f088cf5c5010a17296b3d71d50bdc3d92053114a
iFaDiNg <iFaDiNg@IFADING-PC>
2018-01-21 16:14:01 +0100

    Programmname geändert

--- QuickNetChat.Client/Main.Designer.cs
+++ QuickNetChat.Client/Main.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "Main";
+            this.Text = "QuickNetChat";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
