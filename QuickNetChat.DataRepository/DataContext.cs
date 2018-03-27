896e200bb6cc4e76af62ed9896a5b730bd3e70d3
Christian Groothoff <ruffo324@gmail.com>
2018-03-28 00:29:18 +0200

    #1 - First try to initalize the DataContext in a new thread...

--- QuickNetChat.DataRepository/DataContext.cs
+++ QuickNetChat.DataRepository/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Data;
+using System.Data.Entity;
 using System.Data.Entity.Migrations;
 using QuickNetChat.DataRepository.Entitys;
 
@@ -6,6 +7,15 @@ namespace QuickNetChat.DataRepository
 {
     public class DataContext : DbContext
     {
+        /// <summary>
+        ///     Event delegate for the OnAfterDataContextInitalized event.
+        /// </summary>
+        public delegate void AfterDataContextInitalized();
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Constructor. Calls the DbContext Base function with the DataConsts.ConnectionString.
+        /// </summary>
         public DataContext() : base(DataConsts.ConnectionString)
         {
         }
@@ -19,7 +29,12 @@ namespace QuickNetChat.DataRepository
 
         public DbSet<Media> Media { get; set; }
 
-        internal void FirstInit()
+        public static event AfterDataContextInitalized OnDataContextInitialized;
+
+        /// <summary>
+        ///     Function to initalize the DataContext the first time.
+        /// </summary>
+        internal async void FirstInit()
         {
             Database.SetInitializer<DataContext>(null);
 
@@ -31,10 +46,32 @@ namespace QuickNetChat.DataRepository
                 AutomaticMigrationsEnabled = true
             };
 
+            // Assign database migrator and update it.
             DbMigrator dbMigrator = new DbMigrator(migrationsConfiguration);
             dbMigrator.Update();
 
-            Database.Connection.Open();
+            // Bind event and open the DataContext async.
+            Database.Connection.StateChange += ConnectionOnStateChange;
+            await Database.Connection.OpenAsync().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        ///     Called if the Database.Connection state is changed.
+        /// </summary>
+        /// <param name="sender">Caller object of this event.</param>
+        /// <param name="stateChangeEventArgs">State changed event arguments</param>
+        private void ConnectionOnStateChange(object sender, StateChangeEventArgs stateChangeEventArgs)
+        {
+            if (stateChangeEventArgs.CurrentState == ConnectionState.Open)
+                OnOnDataContextInitialized();
+        }
+
+        /// <summary>
+        ///     Called if the Data context is fully initialized.
+        /// </summary>
+        protected virtual void OnOnDataContextInitialized()
+        {
+            OnDataContextInitialized?.Invoke();
         }
     }
 }
--- QuickNetChat.View.Main/App.xaml.cs
+++ QuickNetChat.View.Main/App.xaml.cs
@@ -1,22 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using QuickNetChat.DataRepository;
 
 namespace QuickNetChat.View.Main
 {
     /// <inheritdoc />
     /// <summary>
-    /// Interaktionslogik für "App.xaml"
+    ///     Interaktionslogik für "App.xaml"
     /// </summary>
     public partial class App : Application
     {
-        public App ()
+        private readonly SplashScreen.MainWindow _splashScreenMainWindow;
+        private bool _startupCompletlyDone;
+
+        public App()
         {
-            Console.WriteLine("ASDASDASD");
+            // Setup application.
+            _splashScreenMainWindow = new SplashScreen.MainWindow();
+            _splashScreenMainWindow.Show();
+
+            // Bind event of database initalization, update progress.
+            DataContext.OnDataContextInitialized += OnOnDataContextInitialized;
+            _splashScreenMainWindow.UpdateProgress("Initalize Database", "Init context.");
+
+            // Initalize data context.
+            DataRepository.DataRepository dataRepository = new DataRepository.DataRepository();
+            dataRepository.GetContext();
+            StartupCompleteDone();
+        }
+
+        private void OnOnDataContextInitialized()
+        {
+            _splashScreenMainWindow.UpdateProgress("Initalize Database", "Init context.");
+            _startupCompletlyDone = true;
+            StartupCompleteDone();
+        }
+
+        private void StartupCompleteDone()
+        {
+            // TODO: Find better solution.
+            if (!_startupCompletlyDone)
+                return;
+
+            // Close splash screen 
+            _splashScreenMainWindow.UpdateProgress("Startup done", "done.");
+            _splashScreenMainWindow.Close();
         }
     }
 }
